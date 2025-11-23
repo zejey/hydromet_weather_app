2d566cd5e1d0bc3889296e71c0b96b4f1acfd4ea
zjayarcena <182246246+zjayarcena@users.noreply.github.com>
2025-11-23 16:55:05 +0800

    Deleted the py/ folder because it's no longer needed

--- lib/py/check_event_balance.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from model import engineer_features, hazard_score
-
-df = pd.read_csv("balanced_training_data.csv")
-df = engineer_features(df)
-df["event"] = df.apply(hazard_score, axis=1)
-print(df["event"].value_counts())
